d671b3fe062431c0768db5a79219b0b0c64e19e4
ariannaal <ariannaloreti25@gmail.com>
2024-06-26 17:06:49 +0200

    adds form

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,17 +1,62 @@
 // Crea un componente BookList. Questo componente riceverà dalle prop un array di libri, e li visualizzerà attraverso un .map() ritornando per ogni elemento il componente SingleBook. Successivamente monta BookList dentro il componente App, e forniscigli una lista di libri da uno dei file .json come prop. Dovresti ottenere a schermo un risultato simile al componente AllTheBooks: ora però la struttura è molto più modulare e riutilizzabile.
 
+ //Inserisci all'interno del componente BookList un campo di ricerca, prima della griglia di libri; se riempito, dovranno venire mostrati solamente i libri il cui titolo contiene il valore cercato (suggerimento: salva la stringa di ricerca dentro allo stato del componente BookList e filtra i libri di conseguenza).
+
 import React from 'react';
 import SingleBook from './SingleBook';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { Component } from 'react';
 
 
-const BookList = ({ books }) => { // riceve un array di libri come prop
+class FormComponent extends Component {
+
+
+  state = {
+    formValue: {
+      bookTitle: ''
+    }
+    };
+    
+  
+
+  render() {
+
+      
+    return (
+      <div>
+        <Form className='formInput mx-auto'>
+          <Form.Group className="mb-3" controlId="formText">
+              <Form.Control
+                type="text"
+                placeholder="Looking for something?"
+                value={this.state.formValue.bookTitle}
+                        onChange={(e) => {
+                            this.setState({
+                                formValue: {
+                                    ...this.state.formValue, // copia dello stato
+                                    bookTitle: e.target.value,
+                        },
+                    })
+                }}
+              />
+            
+                </Form.Group>
+        </Form>
+      </div>
+    );
+  }
+}
+
+const BookList = ({ books }) => { // riceve un array come prop
+
+    
   return (
     <Container>
-      <Row sm={1} md={2} lg={3}  className="g-4">
+      <FormComponent /> 
+      <Row sm={1} md={2} lg={3} className="g-4 my-3 justify-content-center">
         {books.map((book, idx) => (
-          <Col key={idx} className='d-flex justify-content-center cardBook'>
-            <SingleBook book={book} /> 
+          <Col key={idx} className='cardBook d-flex justify-content-center'>
+            <SingleBook book={book} />
           </Col>
         ))}
       </Row>
@@ -20,13 +65,3 @@ const BookList = ({ books }) => { // riceve un array di libri come prop
 };
 
 export default BookList;
-
-
-
-
-
-
-
-
-
-
